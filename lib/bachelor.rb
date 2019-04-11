1da71076b128385019e839771019f628356c2671
James Pier <jpier2012@gmail.com>
2019-04-11 14:14:51 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -12,7 +12,7 @@ def get_first_name_of_season_winner(data, season)
       contestants.each {|person|
         if person[:"status"] == "Winner"
           winner_first_name = person[:"name"].split(" ").first
-          
+
         end
       }
     end

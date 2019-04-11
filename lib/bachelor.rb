5a6c368db048f661f6add4f281ae5a56dffb436e
James Pier <jpier2012@gmail.com>
2019-04-11 14:04:00 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -23,7 +23,7 @@ end
 puts get_first_name_of_season_winner(data_set,"season 10")
 puts get_first_name_of_season_winner(data_set,"season 16")
 puts get_first_name_of_season_winner(data_set,"season 11")
-binding.pry
+#binding.pry
 #####
 #####
 

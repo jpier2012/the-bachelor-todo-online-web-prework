38bd35369e4aaa7fd922464eef76db05004ce93e
James Pier <jpier2012@gmail.com>
2019-04-11 13:44:45 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -9,7 +9,7 @@ def get_first_name_of_season_winner(data, season)
   data.each {|season_name, contestants|
   #binding.pry
   if season.to_sym == season_name
-    contestants.each {|person|
+    contestants.each_with_object {|person, stats|
 
       if person[:"status"] == "Winner"
         winner_first_name == person[:"name"].split(" ")[0]

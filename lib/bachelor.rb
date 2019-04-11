75e00adf54f395c07ad7c6d7d5cb4a57f138ab1a
James Pier <jpier2012@gmail.com>
2019-04-11 15:00:14 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -76,7 +76,7 @@ def get_average_age_for_season(data, season)
   ages = 0
 
   data.each {|season_name, contestants|
-    if season_name == season 
+    if season_name == season
       contestants.each {|person|
         if person["hometown"] == hometown
           occupation = person["occupation"]

c5ba4f1f435890f6f4d2484db4ee39ccaf3e2dd2
James Pier <jpier2012@gmail.com>
2019-04-11 14:20:18 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -21,7 +21,7 @@ def get_first_name_of_season_winner(data_set, season)
   winner_first_name
 end
 
-get_first_name_of_season_winner(data, "season 10")
+puts get_first_name_of_season_winner(data, "season 10")
 #binding.pry
 #####
 #####
